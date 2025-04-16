d9182ca7f49ec4d65682dcd00a52e2b2bc3f1c32
sri <146156147+sriramgg@users.noreply.github.com>
2025-04-16 12:01:41 +0530

    Create firebase-config.js
    ...

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqipYCm7DJwDNhcpqj1IWcInfj_4f1IY",
+  authDomain: "otp-login-github.firebaseapp.com",
+  projectId: "otp-login-github",
+  storageBucket: "otp-login-github.appspot.com",
+  messagingSenderId: "796677514856",
+  appId: "1:796677514856:web:cbd6fd71928c17936b6785",
+  measurementId: "G-D1HCKMRL1S"
+};
+
+firebase.initializeApp(firebaseConfig);
