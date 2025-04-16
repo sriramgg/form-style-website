db051bb334cc768b94d019335ee24305cb359383
sri <146156147+sriramgg@users.noreply.github.com>
2025-04-16 19:15:17 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBqipYCm7DwUhcpgj1WvEnfj_4f1lY",
+  authDomain: "otp-login-github.firebaseapp.com",
+  projectId: "otp-login-github",
+  storageBucket: "otp-login-github.appspot.com",
+  messagingSenderId: "796677514856",
+  appId: "1:796677514856:web:cdb6fd71928c1739607685",
+  measurementId: "G-D1CKHKMLH5"
+};
+
+firebase.initializeApp(firebaseConfig);
